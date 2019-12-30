d9a9596c4b9ee0fc723e690973b8fe28becbb2ef
Jacob Ilias Komissar <jikomissar@wpi.edu>
2019-12-29 23:03:50 -0700

    Update setup.py for PyPI

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 from setuptools import setup
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='dir2pdf',
-    version='1.0.0',
+    version='0.0.1',
     author='Jacob Komissar',
     description='Convert directories of images to PDFs',
-    python_requires='>=3.7',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jirassimok/dir2pdf',
+
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+
     py_modules=['dir2pdf'],
+
+    python_requires='>=3.7',
     install_requires=['Pillow'],
     extras_require={
         'dev': ['flake8'],
